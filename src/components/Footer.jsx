920a20956d94d19d89d17a81cf21fbdce51e1d24
Ibro <ibroabdraimov20@gmail.com>
2025-10-18 11:07:25 +0600

    sadfdsa

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Logo from '../assets/logo.jpg';
+import Logo from '../assets/Logo.jpg';
 
 const Footer = () => {
   const [activeLegal, setActiveLegal] = useState(null);
